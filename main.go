4a48614c07d467a8fb6aa3dcaecdc64d126985df
Abbas Goudarzi <abbas.ce24@gmail.com>
2025-02-25 09:36:05 +0330

    Add Extra functions

--- main.go
+++ main.go
@@ -1,19 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"sort"
 )
 
-type student struct {
-	firstName       string
-	lastName        string
-	mathScore       float32
-	scienceScore    float32
-	literatureScore float32
-	average         float32
+type Student struct {
+	FirstName       string
+	LastName        string
+	MathScore       float32
+	ScienceScore    float32
+	LiteratureScore float32
+	Average         float32
 }
 
-var students []student
+var students []Student
 
 func main() {
 	showMenu()
@@ -24,7 +27,10 @@ func showMenu() {
 	fmt.Println("\nStudent Management System Menu:")
 	fmt.Println("1. Add a student")
 	fmt.Println("2. Display students")
-	fmt.Println("3. Exit")
+	fmt.Println("3. Identify and display the top-performing student")
+	fmt.Println("4. Sort Students by their average scores")
+	fmt.Println("5. Save student data in a text file")
+	fmt.Println("6. Exit")
 	fmt.Print("Please enter your choice: ")
 	fmt.Scanln(&choiceOfUser)
 
@@ -34,6 +40,12 @@ func showMenu() {
 	case "2":
 		displayStudents()
 	case "3":
+		displayTopPerformingStudent()
+	case "4":
+		sortStudents()
+	case "5":
+		saveStudentsDataInJsonFile()
+	case "6":
 		return
 	}
 
@@ -42,7 +54,7 @@ func showMenu() {
 func addStudent() {
 	var firstName, lastName string
 	var mathScore, scienceScore, literatureScore float32
-	fmt.Print("Enter the First Name: ")
+	fmt.Print("\nEnter the First Name: ")
 	fmt.Scanln(&firstName)
 	fmt.Print("Enter the Last Name: ")
 	fmt.Scanln(&lastName)
@@ -53,12 +65,12 @@ func addStudent() {
 	fmt.Print("Enter the Literature Score: ")
 	fmt.Scanln(&literatureScore)
 
-	newStudent := student{
-		firstName:       firstName,
-		lastName:        lastName,
-		mathScore:       mathScore,
-		scienceScore:    scienceScore,
-		literatureScore: literatureScore,
+	newStudent := Student{
+		FirstName:       firstName,
+		LastName:        lastName,
+		MathScore:       mathScore,
+		ScienceScore:    scienceScore,
+		LiteratureScore: literatureScore,
 	}
 
 	students = append(students, newStudent)
@@ -70,13 +82,56 @@ func addStudent() {
 
 func displayStudents() {
 	for i, s := range students {
-		fmt.Printf("%d. %s %s | Math: %.2f | Science: %.2f | Literature: %.2f | Average: %.2f \n", i+1, s.firstName,
-			s.lastName, s.mathScore, s.scienceScore, s.literatureScore, s.average)
+		fmt.Printf("%d. %s %s | Math: %.2f | Science: %.2f | Literature: %.2f | Average: %.2f \n", i+1, s.FirstName,
+			s.LastName, s.MathScore, s.ScienceScore, s.LiteratureScore, s.Average)
 	}
 
 	showMenu()
 }
 
-func calcAverage(s *student) {
-	s.average = (s.mathScore + s.scienceScore + s.literatureScore) / 3
+func calcAverage(s *Student) {
+	s.Average = (s.MathScore + s.ScienceScore + s.LiteratureScore) / 3
+}
+
+func sortStudents() {
+	sort.SliceStable(students, func(i, j int) bool {
+		return students[i].Average > students[j].Average
+	})
+
+	fmt.Println("\nStudents have been sorted by their average scores")
+	showMenu()
+}
+
+func displayTopPerformingStudent() {
+	if len(students) == 0 {
+		fmt.Println("\nYou have not saved any students.")
+	} else {
+		sort.SliceStable(students, func(i, j int) bool {
+			return students[i].Average > students[j].Average
+		})
+
+		s := students[0]
+		fmt.Printf("\n%s %s | Math: %.2f | Science: %.2f | Literature: %.2f | Average: %.2f \n", s.FirstName,
+			s.LastName, s.MathScore, s.ScienceScore, s.LiteratureScore, s.Average)
+	}
+	showMenu()
+}
+
+func saveStudentsDataInJsonFile() {
+
+	if len(students) == 0 {
+		fmt.Println("\nYou have not saved any students.")
+	} else {
+		jsonData, err := json.Marshal(students)
+		if err != nil {
+			panic(err)
+		}
+		err = os.WriteFile("students.json", jsonData, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("\nStudents have been saved in a text file")
+	}
+	showMenu()
 }
